c0be9d251be16fca54145defedb9d96f2d26d0f9
Mahmud Ghali Abdulhamid <mahmudghali01@gmail.com>
2026-01-29 14:16:58 +0100

    implement database connection and user authentication logic

---
+++ controllers/authController.js
@@ -0,0 +1,36 @@
+const passport = require('passport');
+const User = require('../models/User');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+passport.use(new LocalStrategy({ 
+    usernameField: 'username',
+    passwordField: 'password'
+}, async (username, password, done) => {
+    try {
+        const user = await User.findOne({ username });
+        if (!user) return done(null, false, { message: 'Incorrect username.' });
+        
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return done(null, false, { message: 'Incorrect password.' });
+        
+        return done(null, user);
+    } catch (error) {
+        return done(error);
+    }
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (error) {
+        done(error);
+    }
+});
+
+module.exports = passport;
---
+++ controllers/todoController.js
@@ -0,0 +1,105 @@
+const Todo = require('../models/Todo');
+const User = require('../models/User');
+
+// Get all todos for the current user
+exports.getTodos = async (req, res) => {
+    try {
+        const todos = await Todo.find({ user: req.user._id, status: { $ne: 'deleted' } }).sort({ createdAt: -1 });
+        res.render('index', {
+            todos: todos,
+            user: req.user,
+            title: 'Todo List'
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).render('error', { error: 'Server Error' });
+    }
+};
+
+// Create a new todo
+exports.createTodo = async (req, res) => {
+    try {
+        const todo = new Todo({
+            title: req.body.title,
+            user: req.user._id
+        });
+
+        const savedTodo = await todo.save();
+
+        await User.findByIdAndUpdate(req.user._id, {
+            $push: { todos: savedTodo._id }
+        });
+
+        res.redirect('/');
+    } catch (err) {
+        console.error(err);
+        res.redirect('/');
+    }
+};
+
+// Update a todo (toggle completed status)
+exports.toggleComplete = async (req, res) => {
+    try {
+        const todo = await Todo.findOne({ _id: req.params.id, user: req.user._id });
+
+        if (!todo) {
+            return res.status(404).json({ message: 'Todo not found' });
+        }
+
+        todo.status = todo.status === 'completed' ? 'pending' : 'completed';
+        await todo.save();
+
+        res.redirect('/');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+};
+
+// Delete a todo (chnge status to deleted
+exports.deleteTodo = async (req, res) => {
+    try {
+        const todo = await Todo.findOneAndUpdate(
+            { _id: req.params.id, user: req.user._id },
+            { status: 'deleted' }
+        );
+
+        if (!todo) {
+            return res.status(404).json({ message: 'Todo not found' });
+        }
+
+        res.redirect('/');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+};
+
+// Get trashed todos
+exports.getTrashed = async (req, res) => {
+    try {
+        const todos = await Todo.find({ user: req.user._id, status: 'deleted' }).sort({ createdAt: -1 });
+        res.render('trash', {
+            todos: todos,
+            user: req.user,
+            title: 'Trash'
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).render('error', { error: 'Server Error' });
+    }
+};
+
+// Restore from trash
+exports.restoreTodo = async (req, res) => {
+    try {
+        await Todo.findOneAndUpdate(
+            { _id: req.params.id, user: req.user._id },
+            { status: 'pending' }
+        );
+        res.redirect('/trash');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+};
